bdf863c6894f1d23dced4b5056d58daa9255b300
Andreas Pålsson <andreas.palsson@acast.com>
2018-10-24 18:34:15 +0200

    build(fix-docker): Fix docker build script

--- .circleci/buildDocker.sh
+++ .circleci/buildDocker.sh
@@ -8,22 +8,30 @@ set -o errexit
 
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
+cd ./applications
+ROOT_DIR=$(pwd)
+while read -r line ; do
+  echo "Processing $line"
+  # your code goes here
+  TAG=$(echo $line | cut -f2 -d ' ' )
 
-APP_NAME=$(echo $CIRCLE_TAG | cut -f1 -d@)
-VERSION=$(echo $CIRCLE_TAG | cut -f2 -d@)
-DOCKER_TAG=$APP_NAME-$VERSION
-DIRECTORY=./applications/$APP_NAME
-pwd
-if [ -d "$DIRECTORY" ]; then
-  # Control will enter here if $DIRECTORY exists.
-  cd $DIRECTORY
-  pwd
+  APP_NAME=$(echo $TAG | cut -f1 -d@)
+  VERSION=$(echo $TAG | cut -f2 -d@)
+  if [ -n "$APP_NAME" ]; then
+    DOCKER_TAG=$APP_NAME-$VERSION
+    DIRECTORY=./$APP_NAME
 
-  docker build -t palsson/monotest:prod-$DOCKER_TAG .
-  docker tag $(docker images -q palsson/monotest:prod-$DOCKER_TAG) palsson/monotest:$APP_NAME-dev
-  docker tag $(docker images -q palsson/monotest:prod-$DOCKER_TAG) palsson/monotest:$APP_NAME-latest
-  docker login -u $DOCKER_USER -p $DOCKER_PASS
-  docker push palsson/monotest:prod-$DOCKER_TAG
-  docker push palsson/monotest:$APP_NAME-dev
-  docker push palsson/monotest:$APP_NAME-latest
-fi
+    cd $DIRECTORY && {
+        docker build -t palsson/monotest:prod-$DOCKER_TAG .
+        docker tag $(docker images -q palsson/monotest:prod-$DOCKER_TAG) palsson/monotest:$APP_NAME-dev
+        docker tag $(docker images -q palsson/monotest:prod-$DOCKER_TAG) palsson/monotest:$APP_NAME-latest
+        docker login -u $DOCKER_USER -p $DOCKER_PASS
+        docker push palsson/monotest:prod-$DOCKER_TAG
+        docker push palsson/monotest:$APP_NAME-dev
+        docker push palsson/monotest:$APP_NAME-latest
+
+        cd $ROOT_DIR
+    }
+  fi
+done < <(git log --grep='ci(release)' --max-count=1 | grep -)
+
