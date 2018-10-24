f314b193acbf8387ff35b7bd8ce2e4d1bf60c5c3
Andreas Pålsson <andreas.palsson@acast.com>
2018-10-24 15:42:07 +0200

    fix(app1): Update headline

--- applications/app1/src/App.js
+++ applications/app1/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Application 1
           </a>
         </header>
       </div>
